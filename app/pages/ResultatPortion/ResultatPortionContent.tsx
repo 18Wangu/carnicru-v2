edea1158861da5118cf8b32790d5bddee36f9403
Theo Premartin <theopremartin@MacBook-Pro-de-Theo.local>
2025-02-07 14:08:55 +0100

    responsive resultat portion ok

--- app/pages/ResultatPortion/ResultatPortionContent.tsx
+++ app/pages/ResultatPortion/ResultatPortionContent.tsx
@@ -6,7 +6,6 @@ import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { Footer } from "@/components/Footer";
-//import { CodePromo } from "@/components/CodePromo";
 
 const ResultatPortionContent: React.FC = () => {
   const searchParams = useSearchParams();
@@ -32,16 +31,11 @@ const ResultatPortionContent: React.FC = () => {
     },
   ];
 
-
-  const renderPlanCard = (
-    title: string,
-    price: string,
-    imageSrc: string,
-  ) => (
-    <div className="flex gap-10 items-start self-stretch my-auto min-w-[240px] w-[351px]">
-      <div className="flex flex-col items-center h-[420px] min-w-[240px] w-[351px]">
+  const renderPlanCard = (title: string, price: string, imageSrc: string) => (
+    <div className="flex gap-10 my-auto md:w-[351px]">
+      <div className="flex flex-col items-center h-[480px] md:w-[351px]">
         <div className="gap-10 text-[#004339]">{title}</div>
-        <div className="flex overflow-hidden flex-col mt-5 max-w-full text-right whitespace-nowrap rounded-2xl w-[351px]">
+        <div className="flex overflow-hidden flex-col mt-5 rounded-2xl w-[351px] relative">
           <div className="flex relative flex-col items-start pt-40 w-full rounded-2xl aspect-[1.245] max-md:pt-24 max-md:pr-5">
             <Image
               loading="lazy"
@@ -51,7 +45,7 @@ const ResultatPortionContent: React.FC = () => {
               height={440}
               className="object-cover absolute inset-0 size-full"
             />
-            <div className="flex overflow-hidden relative gap-4 items-center p-5 bg-[#149A77] rounded-2xl">
+            <div className="flex overflow-hidden absolute bottom-5 left-10 md:left-0 items-center p-3 md:p-5 bg-[#149A77] rounded-2xl">
               <div className="gap-10 self-stretch my-auto">{price}</div>
             </div>
           </div>
@@ -59,19 +53,17 @@ const ResultatPortionContent: React.FC = () => {
         <Link
           href={{
             pathname: "/pages/Livraison",
-            query: { 
-              prix: (() => {
-                return (price);
-              })(),
-            },
+            query: { prix: price },
           }}
-          className="gap-2.5 self-stretch p-5 mt-5 max-w-full text-center text-white whitespace-nowrap bg-[#B9110F] rounded-2xl w-[351px] hover:bg-[#E30613] focus:outline-none focus:ring-2 focus:ring-[#E30613] focus:ring-offset-2" aria-label={`Choisir ${title}`}
+          className="gap-2.5 p-5 mt-5 text-center text-white bg-[#B9110F] rounded-2xl md:w-full hover:bg-[#E30613] focus:outline-none focus:ring-2 focus:ring-[#E30613] focus:ring-offset-2 mx-auto" 
+          aria-label={`Choisir ${title}`}
         >
           Choisir
         </Link>
-        <div className="text-sm text-center text-[#004339] mt-2">
+        <div className="text-sm text-center text-[#004339] mt-2 w-full px-4 md:whitespace-nowrap">
           <em>dont 20€ de livraison déjà inclus dans le prix</em>
         </div>
+
         <div className="flex gap-10 mt-5 min-h-[0px]" />
       </div>
     </div>
@@ -79,27 +71,27 @@ const ResultatPortionContent: React.FC = () => {
 
   return (
     <div>
-      {/*<CodePromo />*/}
       <div className="flex flex-col items-center font-semibold my-20">
-        <Link href="/pages/SimulationPortion" className='absolute top-32 left-12 text-[#004339] text-xl hover:underline hover:underline-offset-4'>
+        <Link href="/pages/SimulationPortion" className='absolute top-32 left-12 text-[#004339] text-xl hover:underline hover:underline-offset-4 hidden md:block'>
           <span>Retour</span>
         </Link>
+
         {/* Portion Details */}
         <div className="flex flex-col items-center font-semibold">
-          <div className="flex flex-col items-center max-w-full text-center w-[1418px]">
-            <div className="text-2xl text-[#004339] max-md:max-w-full">
+          <div className="flex flex-col items-center text-center md:w-[1418px]">
+            <div className="text-2xl text-[#004339]">
               <span className="font-bold text-[#149A77]">{nomChien}</span> a besoin d&apos;une portion quotidienne de {portion} g par jour
             </div>
-            <div className="flex flex-col items-center pl-5 mt-16 max-w-full text-4xl text-[#B9110F] w-[530px] max-md:mt-10">
+            <div className="flex flex-col items-center pl-5 mt-16 text-4xl text-[#B9110F] md:w-[530px] max-md:mt-10">
               <Image
                 loading="lazy"
                 src="/gamelle.svg"
                 alt="Daily portion visualization"
-                className="object-contain max-w-full aspect-[1.96]"
+                className="object-contain max-w-full aspect-[1.96] w-[200px] md:w-[313px]"
                 width={313}
                 height={313}
               />
-              <div className="mt-8">{portion}g de BARF/jour</div>
+              <div className="text-2xl md:text-4xl mt-8">{portion}g de BARF/jour</div>
             </div>
           </div>
           <div className="flex flex-wrap gap-10 justify-center items-center p-2.5 max-md:max-w-full mt-24">
@@ -107,13 +99,13 @@ const ResultatPortionContent: React.FC = () => {
               loading="lazy"
               src="/forme_abstraite.svg"
               alt="Portion composition illustration"
-              className="object-contain self-stretch my-auto aspect-[1.1] min-w-[240px]"
+              className="object-contain self-stretch my-auto aspect-[1.1] w-[220px] md:w-[271px]"
               width={271}
               height={271}
             />
-            <div className="flex flex-col justify-center items-start self-stretch my-auto min-w-[240px]">
+            <div className="flex flex-col justify-center items-start self-stretch my-auto md:w-[500px]">
               <div className="text-2xl text-center text-[#004339]">Dans cette portion, il y aura :</div>
-              <div className="flex flex-col mt-12 text-xl max-md:mt-10">
+              <div className="flex flex-col items-center md:items-start mt-12 text-xl max-md:mt-10">
                 <div className="text-[#B9110F]">60% de viande crue</div>
                 <div className="mt-4 text-[#149A77]">30% d&apos;os charnus</div>
                 <div className="mt-4 text-[#004339]">10% d&apos;abats et mix légumes</div>
